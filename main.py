3814bf53f2822efab3b0cbc79454acaaaad48ac9
Eric <ericzjchee@gmail.com>
2024-02-09 15:34:56 +0800

    Hi

--- main.py
+++ main.py
@@ -1,28 +1,81 @@
 ##################################################
 '''
 Q1: 
+
+def greet(human):
+   print(f'Hi, {human}')
+
+greet('Alice')
+greet('Bob')
+
 '''
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q2:
+
+
+def greet(name1, name2):
+   print(f'Hi, {name1} and {name2}')
+
+
+greet(name2='Bob', name1='Alex')
+
+
 '''
+
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q3:
+def find_rect_area(width, height):
+    print(f'{width*height}')
+
+find_rect_area(width=12.3, height=45.6)
+
 '''
+
+
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q4:
+
+
 '''
+
+def lyrics(animal):
+    print(f'Bought me a {animal} and the {animal} pleased me, I fed my {animal} under yonder tree')
+
+def cat_goes():
+    print('Cat goes fiddle i fee.')
+def hen_goes():
+    print('Hen goes chimmy-chuck, chimmy-chuck,', end=' ')
+    cat_goes()
+def duck_goes():
+    print('Duck goes quack, quack,', end=' ')
+    hen_goes()
+def goose_goes():
+    print('Goose goes hissy,hissy,', end=' ')
+    duck_goes()
+
+lyrics(animal='cat')
+cat_goes()
+lyrics(animal='hen')
+hen_goes()
+lyrics(animal='duck')
+duck_goes()
+lyrics(animal='goose')
+goose_goes()
 
 # TODO: Write your code here
 
